22af6608338f618d43c9e51920b87105526859ca
yejunhong <yejunhong@xingfeiinc.com>
2021-04-08 17:50:04 +0800

    代码案例

--- yjh-tools-demo/src/main/java/com/yjh/tools/demo/controller/CustomCacheController.java
+++ yjh-tools-demo/src/main/java/com/yjh/tools/demo/controller/CustomCacheController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -16,19 +17,23 @@ import java.util.List;
 @RequestMapping("/custom/cache")
 public class CustomCacheController {
 
+    public static List<Integer> uids = Arrays.asList(1,2);
+
     @RequestMapping("/getUserDtos")
-    public Result<List<UserDto>> getUserDtos() {
-        List<UserDto> userDtos = ((CustomCacheController) AopContext.currentProxy()).buiidUserDtos();
+    public Result<List<UserDto>> getUserDtos(int uid) {
+        List<UserDto> userDtos = ((CustomCacheController) AopContext.currentProxy()).buiidUserDtos(uid);
         return Result.success(userDtos);
     }
 
     /**
      * 用户和以前的Cacheable注解意义
      * 只是value读取redis-cache.properties的app.cache.redis.items中对应的name来设置缓存的时间
+     *
+     * condition 当uid不在集合uids中才走缓存
      * @return
      */
-    @Cacheable(value = "config:", key = "'buiidUserDtos'")
-    public List<UserDto> buiidUserDtos() {
+    @Cacheable(value = "config:", key = "'buiidUserDtos'", condition = "!T(com.yjh.tools.demo.controller.CustomCacheController).uids.contains(#uid)")
+    public List<UserDto> buiidUserDtos(int uid) {
         List<UserDto> userDtos = new ArrayList<>(3);
         userDtos.add(new UserDto(1, "张三"));
         userDtos.add(new UserDto(2, "李四"));
